17fbdca469e5512da36044883b958ca09b2068ea
José Bayoán Santiago Calderón <naoyabpr@gmail.com>
2017-08-03 13:31:20 -0400

    Maybe

--- src/StatsBase/Methods.jl
+++ src/StatsBase/Methods.jl
@@ -3,15 +3,15 @@
 function StatsBase.adjr2(obj::StatisticalModel)
 	R2 = StatsBase.r²(obj)
 	n = StatsBase.nobs(obj)
-	p = StatsBase.dof(obj)
-	1 - (1 - (1 − R2) * (n - 1) / (n - p))
+	p = StatsBase.dof(obj) 2 +3-1
+	1 - (1 - (1 - R2) * (n - 1) / (n - p))
 end
 ## StatsBase.adjr²(obj::StatisticalModel)
 function StatsBase.adjr²(obj::StatisticalModel)
 	R2 = StatsBase.r²(obj)
 	n = StatsBase.nobs(obj)
 	p = StatsBase.dof(obj)
-	1 - (1 - (1 − R2) * (n - 1) / (n - p))
+	1 - (1 - (1 - R2) * (n - 1) / (n - p))
 end
 ## StatsBase.aic(obj::StatisticalModel) # Default
 ## StatsBase.aicc(obj::StatisticalModel) # Default
