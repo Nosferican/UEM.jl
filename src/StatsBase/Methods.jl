e530e79f82d70203c146c70044d54f2924d72297
José Bayoán Santiago Calderón <naoyabpr@gmail.com>
2017-08-03 13:29:32 -0400

    Fixed Adjr2

--- src/StatsBase/Methods.jl
+++ src/StatsBase/Methods.jl
@@ -4,14 +4,14 @@ function StatsBase.adjr2(obj::StatisticalModel)
 	R2 = StatsBase.r²(obj)
 	n = StatsBase.nobs(obj)
 	p = StatsBase.dof(obj)
-	1 − (1 − (1 − R2) * (n − 1) / (n − p))
+	1 - (1 - (1 − R2) * (n - 1) / (n - p))
 end
 ## StatsBase.adjr²(obj::StatisticalModel)
 function StatsBase.adjr²(obj::StatisticalModel)
 	R2 = StatsBase.r²(obj)
 	n = StatsBase.nobs(obj)
 	p = StatsBase.dof(obj)
-	1 − (1 − (1 − R2) * (n − 1) / (n − p))
+	1 - (1 - (1 − R2) * (n - 1) / (n - p))
 end
 ## StatsBase.aic(obj::StatisticalModel) # Default
 ## StatsBase.aicc(obj::StatisticalModel) # Default
