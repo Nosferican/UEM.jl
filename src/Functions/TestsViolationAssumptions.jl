700146985b7853023c4cffcb42cbd14d0a9bfae5
José Bayoán Santiago Calderón <naoyabpr@gmail.com>
2017-08-04 00:15:58 -0400

    Let's see

--- src/Functions/TestsViolationAssumptions.jl
+++ src/Functions/TestsViolationAssumptions.jl
@@ -32,3 +32,26 @@ function vif(obj::UnobservedEffectsModel)
 	@printf "Variance Inflation Factor:\n"
 	output = StatsBase.CoefTable([VIF], ["VIF"], Varlist)
 end
+function fe_or_re(fm::DataFrames.Formula, df::DataFrames.DataFrame; PID::Symbol = names(df)[1], TID::Symbol = names(df)[2], contrasts = Dict{Symbol, DataFrames.ContrastsMatrix}())
+	Between = uem(:BE, fm, df, PID = PID, TID = TID, contrasts = contrasts)
+	FixedEffects = uem(:FE, fm, df, PID = PID, TID = TID, contrasts = contrasts)
+	βfe = StatsBase.coef(FixedEffects)
+	ybe = StatsBase.model_response(Between)
+	Xbe = get(model, :X)
+	cᵢ = ybe - Xbe * βfe
+	Bread = inv(cholfact(Xbe' * Xbe))
+	β = Bread * Xbe' * ybe
+	ŷ = Xbe * β
+	û = cᵢ - ŷ
+	mdf = length(β) - 1
+	rdf = (reduce(-,size(Xbe)) + 1)
+	RSS = sum(û.^2)
+	MRSS = sum(û.^2) / rdf
+	MESS = sum((ŷ - mean(cᵢ)).^2) / mdf
+	F = MESS / MRSS
+	F_dist = Distributions.FDist(mdf, rdf)
+	F_value = Distributions.ccdf(F_dist, F)
+	@printf "Esarey and Jaffe (2017) Direct Test for Consistency of Random Effects Model\n
+	F(%.0f, %.0f) = %.2f\n
+	Prob > F = %.4f\n" mdf rdf F F_value
+end
