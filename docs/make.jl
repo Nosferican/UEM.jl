141a6f9873adefb8e1a953b06f27caa92da8ea36
José Bayoán Santiago Calderón <naoyabpr@gmail.com>
2017-08-10 22:06:29 -0400

    Travis?

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,14 @@
 using Documenter, UEM
 
-makedocs()
+makedocs(
+    ...,
+    format = :html,
+    sitename = "Unobserved Effects Models"
+)
+
+deploydocs(
+    repo   = "https://github.com/JuliaEconometrics/UEM.jl.git",
+    target = "build",
+    deps   = nothing,
+    make   = nothing
+)
