192837093c739d40aef560938bf87fcbb72f2a80
José Bayoán Santiago Calderón <naoyabpr@gmail.com>
2017-08-14 19:28:40 -0400

    Added Pygments

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, UEM
 makedocs()
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    deps = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
     repo = "github.com/JuliaEconometrics/UEM.jl.git",
     julia  = "0.6"
 )
