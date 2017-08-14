ee8a50247861460d77fcf814d189b72f5be56d35
José Bayoán Santiago Calderón <naoyabpr@gmail.com>
2017-08-14 17:20:27 -0400

    Docs?

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,9 @@
 using Documenter, UEM
 
 makedocs()
+
+deploydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    repo = "github.com/JuliaEconometrics/UEM.jl.git",
+    julia  = "0.6"
+)
