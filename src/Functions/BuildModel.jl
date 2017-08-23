0c7f5e658c1ec9c57a797649fe5380082890fb42
José Bayoán Santiago Calderón <naoyabpr@gmail.com>
2017-08-23 14:40:04 -0400

    .

--- src/Functions/BuildModel.jl
+++ src/Functions/BuildModel.jl
@@ -20,7 +20,7 @@ function build_model(estimator::Estimators, PID::Vector{Vector{Int64}}, TID::Vec
 	TID = ModelValues_TemporalID(TID)
 	T = ModelValues_T(PID)
 	n = ModelValues_n(PID)
-	Bread = ModelValues_Bread(X, Intercept)
+	Bread = ModelValues_Bread(X)
 	β = ModelValues_β(X, Bread, y)
 	ŷ = ModelValues_ŷ(X, β)
 	û = ModelValues_û(y, ŷ)
@@ -55,8 +55,8 @@ function build_model(estimator::Estimators, PID::Vector{Vector{Int64}}, TID::Vec
 	return PID, TID, X, Bread, y, β, varlist, ŷ, û, nobs, N, n, T, mdf, rdf, RSS, MRSS, individual, idiosyncratic, θ
 end
 function build_model(estimator::RE, PID::Vector{Vector{Int64}}, TID::Vector{Vector{Int64}}, Effect::Symbol, X::Matrix{Float64}, y::Vector{Float64}, varlist::Vector{String}, Categorical::Vector{Bool}, Intercept::Bool)
-	MRSS_be, X̄, ȳ = build_model(BE(), PID, TID, Effect, X, y, varlist, Categorical, Intercept, zero(Float64), short = true)
-	MRSS_fe, T, nobs, N, n, Effect, TID = build_model(FE(), PID, TID, Effect, X, y, varlist, Categorical, Intercept, zero(Float64), short = true)
+	MRSS_be, X̄, ȳ = build_model(BE(), PID, TID, Effect, X, y, varlist, Categorical, Intercept, short = true)
+	MRSS_fe, T, nobs, N, n, Effect, TID = build_model(FE(), PID, TID, Effect, X, y, varlist, Categorical, Intercept, short = true)
 	idiosyncratic = ModelValues_Idiosyncratic(get(MRSS_fe))
 	T = ModelValues_T(T)
 	individual = ModelValues_Individual(MRSS_be, idiosyncratic, T)
@@ -69,7 +69,7 @@ function build_model(estimator::RE, PID::Vector{Vector{Int64}}, TID::Vector{Vect
 	y -= mapreduce(times_row -> repeat([ last(times_row) ], inner = first(times_row)), vcat, Iterators.zip(Lens, get(ȳ) .* get(θ)))
 	y = ModelValues_y(y)
 	varlist = ModelValues_Varlist(varlist[LinearIndependent])
-	Bread = ModelValues_Bread(X, Intercept)
+	Bread = ModelValues_Bread(X)
 	β = ModelValues_β(X, Bread, y)
 	ŷ = ModelValues_ŷ(X, β)
 	û = ModelValues_û(y, ŷ)
@@ -108,7 +108,7 @@ function build_model(estimator::Estimators, PID::Vector{Vector{Int64}}, TID::Vec
 	X̃ = X̃[:,LinearIndependent]
 	X̂ = ModelValues_X(X̂)
 	X̃ = ModelValues_X(X̃)
-	Bread = ModelValues_Bread(X̂, Intercept)
+	Bread = ModelValues_Bread(X̂)
 	y = ModelValues_y(y)
 	nobs = ModelValues_nobs(y)
 	PID = transformID(estimator, PID)
@@ -150,10 +150,9 @@ function build_model(estimator::Estimators, PID::Vector{Vector{Int64}}, TID::Vec
 	θ = ModelValues_θ(idiosyncratic, individual, PID)
 	return PID, TID, X̂, Bread, y, β, varlist, ŷ, û, nobs, N, n, T, mdf, rdf, RSS, MRSS, individual, idiosyncratic, θ
 end
-
 function build_model(estimator::RE, PID::Vector{Vector{Int64}}, TID::Vector{Vector{Int64}}, Effect::Symbol, X::Matrix{Float64}, z::Matrix{Float64}, Z::Matrix{Float64}, y::Vector{Float64}, varlist::Vector{String}, Categorical::Vector{Bool}, CategoricalIV::Vector{Bool}, Intercept::Bool)
-	MRSS_be, X̄, Z̄, z̄, ȳ = build_model(BE(), PID, TID, Effect, X, z, Z, y, varlist, Categorical, CategoricalIV, Intercept, zero(Float64), short = true)
-	MRSS_fe, T, nobs, N, n, Effect, TID = build_model(FE(), PID, TID, Effect,  X, z, Z, y, varlist, Categorical, CategoricalIV, Intercept, zero(Float64), short = true)
+	MRSS_be, X̄, Z̄, z̄, ȳ = build_model(BE(), PID, TID, Effect, X, z, Z, y, varlist, Categorical, CategoricalIV, Intercept, short = true)
+	MRSS_fe, T, nobs, N, n, Effect, TID = build_model(FE(), PID, TID, Effect,  X, z, Z, y, varlist, Categorical, CategoricalIV, Intercept, short = true)
 	idiosyncratic = ModelValues_Idiosyncratic(get(MRSS_fe))
 	T = ModelValues_T(T)
 	individual = ModelValues_Individual(MRSS_be, idiosyncratic, T)
@@ -179,7 +178,7 @@ function build_model(estimator::RE, PID::Vector{Vector{Int64}}, TID::Vector{Vect
 	X̃ = ModelValues_X(X̃)
 	y = ModelValues_y(y)
 	varlist = ModelValues_Varlist(varlist[find(LinearIndependent[1:length(varlist)])])
-	Bread = ModelValues_Bread(X̂, Intercept)
+	Bread = ModelValues_Bread(X̂)
 	β = ModelValues_β(X̂, Bread, y)
 	ŷ = ModelValues_ŷ(X̃, β)
 	û = ModelValues_û(y, ŷ)
--- src/Structs/Values.jl
+++ src/Structs/Values.jl
@@ -10,7 +10,7 @@ struct ModelValues_X <: ModelValues
 end
 struct ModelValues_Bread <: ModelValues
 	value::Matrix{Float64}
-	function ModelValues_Bread(X::ModelValues_X, Intercept::Bool)
+	function ModelValues_Bread(X::ModelValues_X)
 		X = get(X)
 		@assert rank(X) == size(X, 2) "Model Matrix is not full rank.\n"
 		Bread = inv(cholfact(X' * X))
